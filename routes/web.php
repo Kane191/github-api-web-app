0bb038d0e3cac2efe52d20d18f42b2bd1e36f017
sarah <sarakane17@gmail.com>
2021-02-12 17:18:31 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/','App\Http\Controllers\GithubController@index')->name('index');
-Route::get('/dd','App\Http\Controllers\GithubController@dd')->name('dd');
+//Route::get('/dd','App\Http\Controllers\GithubController@dd')->name('dd');
 Route::get('/details/{id}','App\Http\Controllers\GithubController@details')->name('details');
 Route::get('/favourite/{id}','App\Http\Controllers\GithubController@favourite')->name('favourite');
